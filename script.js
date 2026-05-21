55a2bb3737bca4aa0724d90e54caad8567adf406
ehtashamh161 <ehtashamh161@gmail.com>
2026-05-21 09:20:40 +0500

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerText = "💖";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 10 + "px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 6000);
+}
+
+setInterval(createHeart, 300);
